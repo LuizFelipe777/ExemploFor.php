f11a9968f629db4f4c0dad36f7bb2504f003e07c
Luiz Felipe <luizfelipemourab@gmail.com>
2023-05-31 12:07:56 -0300

    Add files via upload

---
+++ Exemplofor.php
@@ -0,0 +1,20 @@
+<?php
+echo "Soma dos números \n";
+$soma = 0;
+
+for ($x = 0; $x <= 20; $x ++) {
+    $soma = $soma + $x;
+}
+echo $soma . "\n";
+
+echo "....................................................................\n";
+
+echo "Tabuada de um número \n";
+
+$numTabuada = 6;
+for ($i = 0; $i <= 10; $i ++){
+    
+     echo $i * $numTabuada . "\n";
+    
+}
+?>
